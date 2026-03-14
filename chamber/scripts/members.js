ede66e7fba86514a91d90223a7f0431b6e311725
unknown <diegoprogra28@gmail.com>
2026-03-14 15:05:24 -0300

    W02 assignment: testing001

---
+++ chamber/scripts/members.js
@@ -0,0 +1,68 @@
+const url = '../members.json';
+
+
+async function getMembersData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data.businesses);
+}
+
+const cards = document.querySelector('#cards');
+
+
+getMembersData();
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        
+
+        let card = document.createElement("section");
+        let name = document.createElement("h2");
+        let image = document.createElement("img");
+        let address = document.createElement("p");
+        let phone = document.createElement("p");
+        let website = document.createElement("a");
+
+
+
+        // Build the h2 
+        name.textContent = member.company_name; 
+        // Build the image by setting all the relevant attributes
+        image.setAttribute('src', `images/${member.image_file}`);
+        image.setAttribute('alt', `Image of ${member.company_name}`); 
+        image.setAttribute('loading', 'lazy');
+        image.setAttribute('width', '340');
+        image.setAttribute('height', '440');
+
+        address.innerHTML = `${member.company_address}`;
+        phone.innerHTML = `${member.company_phone}`;
+        website.innerHTML = `<a href="${member.company_website}" target="_blank">Visit Website</a>`;
+
+        // Append the section(card) with the created elements
+        card.appendChild(name);
+        card.appendChild(image);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+
+        cards.appendChild(card);
+    }); // end of arrow function and forEach loop
+}
+
+
+// Grid and List Toggle
+
+const gridButton = document.querySelector('#grid');
+const listButton = document.querySelector('#list');
+
+
+gridButton.addEventListener("click", () => {
+    cards.classList.add("grid");
+    cards.classList.remove("list");
+});
+
+listButton.addEventListener("click", () => {
+    cards.classList.add("list");
+    cards.classList.remove("grid");
+});
+
