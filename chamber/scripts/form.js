fd204585740d88c7e48dc01697a319ff58fbfa64
unknown <diegoprogra28@gmail.com>
2026-03-28 16:16:52 -0300

    W04 assigment 006

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -2,6 +2,6 @@ const form = document.querySelector("form");
 const timestampInput = document.getElementById("timestamp");
 
 
-FormData.addEventListener("submit", () => {
+form.addEventListener("submit", () => {
     timestampInput.value = new Date().toISOString();
 });
